a8b708a32cd3c56f189dedacf8d37aa08e033400
Volkan K <vkaraca79@hotmail.com>
2021-09-09 15:58:01 +0300

    added more hello feature

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,6 +2,8 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Developing hello future");
+        System.out.println("Local Repo is already published to GitHub");
+        System.out.println("Doing more work");
     }
 
 }
